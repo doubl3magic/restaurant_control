376e6ab1951a849b4385337098287056010b2a8e
Ivaylo Stoykov <33195728+doubl3magic@users.noreply.github.com>
2024-01-06 16:15:17 +0200

    Configuring admin page

--- resturant_control/auth_app/admin.py
+++ resturant_control/auth_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from resturant_control.auth_app.models import Profile
+
+UserProfile = get_user_model()
+
+
+@admin.register(UserProfile)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'date_joined')
+    search_fields = ('username',)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'user')
